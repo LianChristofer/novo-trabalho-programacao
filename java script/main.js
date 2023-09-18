1e2ff4b023c492790d428a85d2b435044749be5a
gabryelluz <130998782+gabryelluz@users.noreply.github.com>
2023-09-18 08:23:43 -0300

    k

--- java script/main.js
+++ java script/main.js
@@ -7,7 +7,7 @@ const ListaDeTeclas = document.querySelectorAll('.tecla');
 let contador = 0;
 
 while (contador < ListaDeTeclas.length) {
-    ListaDeTeclas[0].onclick = Function() {
+    ListaDeTeclas[0].onclick = function () {
         Tocar(ListaDeTeclas);
     }
     contador = contador + 1;
