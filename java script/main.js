44e4f4bb49962b378cd719e29222d2602d5353e3
gabryelluz <130998782+gabryelluz@users.noreply.github.com>
2023-09-18 07:44:45 -0300

    1

--- java script/main.js
+++ java script/main.js
@@ -6,7 +6,7 @@ const ListaDeTeclas = document.querySelectorAll('.tecla');
 
 let contador = 0;
 
-while (contador < 9) {
+while (contador < ListaDeTeclas.length) {
     ListaDeTeclas[0].onclick = TocaSomPom;
     contador = contador + 1;
     console.log(contador);
