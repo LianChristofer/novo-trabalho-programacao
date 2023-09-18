293d718a0a9bf7071cf5d4952435084788917ffd
gabryelluz <130998782+gabryelluz@users.noreply.github.com>
2023-09-18 08:25:22 -0300

    d

--- java script/main.js
+++ java script/main.js
@@ -7,7 +7,7 @@ const ListaDeTeclas = document.querySelectorAll('.tecla');
 let contador = 0;
 
 while (contador < ListaDeTeclas.length) {
-    ListaDeTeclas[0].onclick = function () {
+    ListaDeTeclas[contador].onclick = function () {
         Tocar('#som-tecla-toim');
     }
     contador = contador + 1;
